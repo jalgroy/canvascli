7451ab44083bf73be917d74883947ed70564f368
Petter Daae <petter.daae@gmail.com>
2019-04-29 18:57:36 +0200

    change assignments output format

--- canvascli/commands/assignments.py
+++ canvascli/commands/assignments.py
@@ -5,6 +5,10 @@ def assignments(canvas, course):
     assignments = course.get_assignments()
 
     for assignment in assignments:
-        print_title(f"{assignment.name}")
-        print_info(f"{assignment.html_url}")
+        print_title(assignment.name)
+        # print_html(assignment.attributes['description'])
         print_info(f"due: {assignment.due_at}")
+        print_info(f"submitted: {assignment.has_submitted_submissions}")
+        print_info(f'published: {assignment.published}')
+        print_url(assignment.html_url)
+        print()
