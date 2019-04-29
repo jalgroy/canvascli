d1fbd26c1caa7cbecf6616fd4a2a6092ec258b13
Petter Daae <petter.daae@gmail.com>
2019-04-29 15:16:41 +0200

    add config.json

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+Usage:
+    canvascli.py courses
+    canvascli.py assignments <course>
+    canvascli.py announcements <course>
+'''
+
+from docopt import docopt
+from canvasapi import Canvas
+import json
+
+def readConfig():
+    with open('./config.json') as json_file:
+        data = json.load(json_file)
+        return data
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    config = readConfig()
+    canvas = Canvas(config["api_url"], config["api_key"])
